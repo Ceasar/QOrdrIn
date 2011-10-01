c56621d4e0b315986db13dc0ea4dbec5a83fbddc
Sachi Shah <sachi.a.shah@gmail.com>
2011-10-01 19:01:49 -0400

    set up ordrin stuff

--- settings.py
+++ settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'media',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
